d5595210c636df5b7ec07a1da2c79a9cdc305549
長谷川司 <asakust.awagesah@gmail.com>
2025-06-08 23:19:53 +0900

    first commit

---
+++ SwiftMacroSample/SwiftMacroSample/SwiftMacroSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMacroSampleApp.swift
+//  SwiftMacroSample
+//
+//  Created by 長谷川司 on 2025/06/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMacroSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftMacroSample/SwiftMacroSampleTests/SwiftMacroSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMacroSampleTests.swift
+//  SwiftMacroSampleTests
+//
+//  Created by 長谷川司 on 2025/06/08.
+//
+
+import Testing
+@testable import SwiftMacroSample
+
+struct SwiftMacroSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
